485d70a2df6ccb2fb065d106d05683ececc4be4b
danjessen <danjessen@gmail.com>
2013-05-13 16:31:40 +0200

    moved around files, basic typography implementation

--- build/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# compiles assets/less to assets/css
-
-# needs npm install -g less
-lessc assets/less/screen.less assets/css/style.css
-lessc --yui-compress assets/less/screen.less assets/css/style.min.css
-echo "done"
-
