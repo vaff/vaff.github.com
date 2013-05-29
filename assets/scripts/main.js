834952ed65a06254e5d459d45d1ec4d5011dd62f
danjessen <danjessen@gmail.com>
2013-05-29 16:30:40 +0200

    build file changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,7 +10,7 @@ require.config({
     }
   },
   paths: {
-    jquery: ["//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min", "./lib/jquery.1.9.1.min.js"]
+    jquery: ["//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min", "./lib/jquery-1.9.1.min.js"]
   }
 });
 
