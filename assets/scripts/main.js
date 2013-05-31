7b68679c6e5683b9552ef6c8cf47ea42191ed647
danjessen <danjessen@gmail.com>
2013-05-31 08:59:15 +0200

    build changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,18 @@
 "use strict";
 
-require.config({
-  baseUrl: "/assets/scripts",
-  packages: ["app"],
-  shim: {
-    "lib/jquery.fittext": {
-        deps: ["jquery"],
-        exports: "jQuery.fn.fitText"
-    }
-  },
-  paths: {
-    jquery: ["//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min", "./lib/jquery-1.9.1.min.js"]
-  }
-});
+// require.config({
+//   baseUrl: "/assets/scripts",
+//   packages: ["app"],
+//   shim: {
+//     "lib/jquery.fittext": {
+//         deps: ["jquery"],
+//         exports: "jQuery.fn.fitText"
+//     }
+//   },
+//   paths: {
+//     jquery: ["//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min", "./lib/jquery-1.9.1.min.js"]
+//   }
+// });
 
 
 require([
